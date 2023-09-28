4331b51ed5c8ab9b29020a3c6b2f470c76843345
SouvikDas2002 <dev.souvik2002@gmail.com>
2023-09-29 02:17:09 +0530

    update password security

--- login.js
+++ login.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const login=express.Router();
 const fs=require('fs');
+const bcrypt=require('bcrypt');
+const { equal } = require('assert');
 
 login.get("/", (req, res) => {
     if (req.session.email && req.session.role=="user")
@@ -18,45 +20,55 @@ login.get('/login',(req, res) => {
     else res.render('login',{message:""});
   });
   
-login.post("/login", (req, res) => {
-    //   console.log(req.body);
+login.post("/login", async(req, res) => {
+  
+    // let checkPass=await bcrypt.hash(req.body.password,10);
+    
       if (req.body.role != "user") {
-        fs.readFile("admin.json", "utf-8", (err, data) => {
+        fs.readFile("admin.json", "utf-8", async(err, data) => {
           let records = JSON.parse(data);
-          let results = records.filter((item) => {
-            if (
-              item.email == req.body.email &&
-              item.password == req.body.password &&
-              item.role == req.body.role
-            )
+          let results = await Promise.all(records.map(async (item) => {
+            const checkPass = await bcrypt.compare(req.body.password, item.password);
+            if (item.email === req.body.email && checkPass && item.role === req.body.role) {
               return true;
-          });
-          if (results.length == 0) res.render('login',{message:"Invalid email/Password"});
+            }
+            return false;
+          }));   
+          console.log(results);
+
+          if(results.includes(true)){
+            req.session.email=req.body.email;
+               req.session.role=req.body.role;
+               console.log("role "+req.session.role);
+            res.redirect(`/admin/dashboard/:${req.session.email}`)
+          }
           else {
-            req.session.email = req.body.email;
-            req.session.role = req.body.role;
-            console.log("role "+req.session.role);
-            res.redirect(`/admin/dashboard/:${req.session.email}`);
-            // res.send("hii admin")
+            res.render('login');
+               
           }
         });
       } else if(req.body.role=="user"){
-        fs.readFile("users.json", "utf-8", (err, data) => {
+        fs.readFile("users.json", "utf-8",async (err, data) => {
           let records = JSON.parse(data);
-          let results = records.filter((item) => {
-            if (
-              item.email == req.body.email &&
-              item.password == req.body.password &&
-              item.role==req.body.role
-            )
+      
+          let results = await Promise.all(records.map(async (item) => {
+            const checkPass = await bcrypt.compare(req.body.password, item.password);
+            if (item.email === req.body.email && checkPass && item.role === req.body.role) {
               return true;
-          });
-          if (results.length == 0) res.render('login',{message:"Invalid email/password"});
-          else {
-               req.session.email=req.body.email;
+            }
+            return false;
+          }));   
+          console.log(results);
+
+          if(results.includes(true)){
+            req.session.email=req.body.email;
                req.session.role=req.body.role;
                console.log("role "+req.session.role);
             res.redirect(`/users/dashboard/:${req.session.email}`)
+          }
+          else {
+            res.render('login');
+               
             // res.send("welcome user");
           }
         });
--- signup.js
+++ signup.js
@@ -1,14 +1,16 @@
-const express=require('express');
-const signUp=express.Router();
-const fs=require('fs');
+const express = require("express");
+const signUp = express.Router();
+const fs = require("fs");
+const bcrypt=require('bcrypt');
 
-signUp.get('/',(req,res)=>{
-    res.render('signUp');
-})
-signUp.post("/", (req, res) => {
-    // console.log(req.body);
+signUp.get("/", (req, res) => {
+  res.render("signUp");
+});
+signUp.post("/", async(req, res) => {
+  // console.log(req.body);
+  let hashedPass= await bcrypt.hash(req.body.password,10);
   
-    if(req.body.role=="user"){
+  if (req.body.role == "user") {
     fs.readFile("users.json", "utf-8", (err, data) => {
       // console.log(data);
       let newUser;
@@ -18,63 +20,76 @@ signUp.post("/", (req, res) => {
       } else {
         oldrecord = JSON.parse(data);
       }
-  
       //* checking user exist or not
       let results = oldrecord.filter((item) => {
-        if (item.email == req.body.email && item.password == req.body.password && item.role == req.body.role)
+        if (
+          item.email == req.body.email &&
+          item.role == req.body.role
+          )
           return true;
-        // else return false;
+        });
+        
+        if (results.length != 0) {
+          console.log("user already present please login");
+        } else {
+          // console.log(hashedPass);
+          let userCredentials=
+          {
+            email:req.body.email,
+            password:hashedPass,
+            role:req.body.role
+          }
+          
+        // console.log(credentials);
+        newUser = oldrecord;
+        newUser.push(userCredentials);
+
+        // console.log(newUser);
+        fs.writeFile("users.json", JSON.stringify(newUser), (err) => {
+          if (err) throw err;
+          else console.log(`user signUp successful`);
+        });
+      }
+    });
+    res.redirect("/login");
+  } else {
+    fs.readFile("admin.json", "utf-8", (err, data) => {
+      // console.log(data);
+      let newUser;
+      let oldrecord;
+      if (data == "") {
+        oldrecord = [];
+      } else {
+        oldrecord = JSON.parse(data);
+      }
+      // * checking admin exist or not
+      let results = oldrecord.filter((item) => {
+        if (item.email == req.body.email &&item.role == req.body.role)
+          return true;
+        
       });
       // console.log(results.length);
-      if (results.length!=0) {
+      if (results.length != 0) {
         console.log("user already present please login");
-      }
-      else{
-      newUser = oldrecord;
-      newUser.push(req.body);
-  
-      // console.log(newUser);
-      fs.writeFile("users.json", JSON.stringify(newUser), (err) => {
-        if (err) throw err;
-        else console.log(`user signUp successful`);
-      });
-  }
-    });
-    res.redirect('/login');
-  }
-  else{
-      fs.readFile("admin.json", "utf-8", (err, data) => {
-          // console.log(data);
-          let newUser;
-          let oldrecord;
-          if (data == "") {
-            oldrecord = [];
-          } else {
-            oldrecord = JSON.parse(data);
+      } else {
+        let adminCredentials=
+          {
+            email:req.body.email,
+            password:hashedPass,
+            role:req.body.role
           }
-          // * checking admin exist or not
-          let results = oldrecord.filter((item) => {
-            if (item.email == req.body.email && item.password == req.body.pass)
-              return true;
-            else return false;
-          });
-          // console.log(results.length);
-          if (results.length!=0) {
-            console.log("user already present please login");
-          }
-          else{
-          newUser = oldrecord;
-          newUser.push(req.body);
-      
-          // console.log(newUser);
-          fs.writeFile("admin.json", JSON.stringify(newUser), (err) => {
-            if (err) throw err;
-            else console.log(`admin signUp successful`);
-          });
-      }
-        });
-        res.redirect('/login');
-  }
-  });
+        newUser = oldrecord;
+        newUser.push(adminCredentials);
 
-module.exports=signUp;
+        // console.log(newUser);
+        fs.writeFile("admin.json", JSON.stringify(newUser), (err) => {
+          if (err) throw err;
+          else console.log(`admin signUp successful`);
+        });
+      }
+    });
+    res.redirect("/login");
+  }
+});
+
+module.exports = signUp;
